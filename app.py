7c4d595e45deb47425810c90d74d2f302f726926
菊池瑞貴 <kikuchimizuki@kikuchimizukinonotopasokon.local>
2025-05-05 00:48:41 +0900

    Fix gunicorn configuration and dependencies

--- app.py
+++ app.py
@@ -664,4 +664,4 @@ def handle_exception(error):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 3001))
-    app.run(host='0.0.0.0', port=port) 
+    app.run(host='0.0.0.0', port=port, debug=False) 
